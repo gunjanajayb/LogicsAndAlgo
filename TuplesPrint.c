0c405bdf32e6477be46bc5e2322c4dba626843e4
Gunjan Bhavsar <gunjanajayb111222@gmail.com>
2021-08-30 10:19:40 +0530

    Added description about the logic implemented in TuplesPrint.c

--- TuplesPrint.c
+++ TuplesPrint.c
@@ -1,12 +1,39 @@
+/* 
+This is the program to print tuples using 2 character/digit into n places.
+For example: as of now 2 characters are 'A' and 'B' (it can be any digit also)
+if "arrs" macro is 4 then it will print following output
+
+AAAA
+AAAB
+AABA
+AABB
+ABAA
+ABAB
+ABBA
+ABBB
+BAAA
+BAAB
+BABA
+BABB
+BBAA
+BBAB
+BBBA
+BBBB
+
+This output is permutaion of putting 2 character/digit in 4 places.
+i.e. there are 16 ways of creating permuation of 2 characters/digit
+for 4 places. It is called tuples
+*/
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #define arrs 4
-int cnt = 0;
 
 void tuple(char a, char b, int c,char* arr, int loc)
 {
     if(c == 0){
-        cnt++;
         printf("%s\n",arr);
         return;}
     
@@ -29,7 +56,6 @@ int main()
     
     
     tuple(a,b,c,arr,loc);
-    printf("cnt = %d",cnt);
 
     return 0;
 }
